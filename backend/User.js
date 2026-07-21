d494588b40953ab11a472e8cbed33a739ab9f714
Mohit Singh <mohit31025@gmail.com>
2026-07-21 22:54:43 +0530

    updated user mongoose schema to include classGrade and stream

--- backend/User.js
+++ backend/User.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  role: { type: String, enum: ['student', 'teacher'], required: true },
+const userSchema = new mongoose.Schema({
+  role: { type: String, required: true },
   fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  classGrade: { type: String, required: true }, // 👈 Added
+  stream: { type: String, default: 'N/A' },      // 👈 Added
+  subjects: [String],
+  mode: { type: String, required: true },
+  email: { type: String, required: true },
   phone: { type: String, required: true },
-  subjects: [{ type: String, required: true }],
-  mode: { type: String, enum: ['home_tutor', 'coaching_class'], required: true },
   locationOrAddress: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
